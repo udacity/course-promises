d556b9104fa9abfd13f92974b1c4e6e667c1cc32
cameronwp <cameron.w.pittman@gmail.com>
2015-11-23 15:34:15 -0800

    adds data categories to polymer elements

--- parse_exoplanets.py
+++ parse_exoplanets.py
@@ -1,13 +1,13 @@
 #!/usr/local/bin/python
 
+# API: http://exoplanetarchive.ipac.caltech.edu/docs/API_exoplanet_columns.html
+
 # import requests
 import csv
 import json
 
 jsonfile = open('file.json', 'w')
 csvfile = open('data.csv', 'rb')
-
-# r = requests.get('http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,pl_letter,ra,dec&order=pl_hostname')
 
 # earth-like
 # http://www.space.com/30172-six-most-earth-like-alien-planets.html
@@ -19,3 +19,7 @@ fieldnames = ['pl_hostname','pl_letter','ra','dec']
 reader = csv.DictReader(csvfile, fieldnames)
 out = json.dumps([row for row in reader])
 jsonfile.write(out)
+
+# for names - csv to json, altogether in data
+#   - include a search parameter: something like "habitable"
+# for data - csv to json, each planet individually in data/planets
