41c399fc73f03a5ff162f73ad13f0d64ba474bef
cameronwp <cameron.w.pittman@gmail.com>
2015-11-30 19:51:49 -0800

    Revert "Revert "adds full dataset""
    This reverts commit 209cb7ba95933c27d5fe76ba6207e9f88b98185d.

--- parse_exoplanets.py
+++ parse_exoplanets.py
@@ -3,12 +3,12 @@ import csv
 import json
 import os
 
-jsondir = 'ExoplanetExplorer/app/data/planets'
-# jsonfile = open('app/data/earth-like-data.json', 'w')
-csvfile = open('earth-like-data.csv', 'rb')
+# jsondir = 'ExoplanetExplorer/app/data/planets'
+jsonfile = open('ExoplanetExplorer/app/data/data.json', 'w')
+csvfile = open('data.csv', 'rb')
 
-if not os.path.exists(jsondir):
-    os.makedirs(jsondir)
+# if not os.path.exists(jsondir):
+#     os.makedirs(jsondir)
 
 # API: http://exoplanetarchive.ipac.caltech.edu/docs/API_exoplanet_columns.html
 
@@ -21,7 +21,31 @@ if not os.path.exists(jsondir):
 fieldnames = ['pl_name','pl_rade','pl_masse','pl_radj','pl_massj','pl_dens','st_dist','pl_disc','pl_telescope','pl_eqt','pl_discmethod','pl_facility','pl_mnum','pl_pelink','pl_edelink','pl_cbflag','pl_orbper','pl_pnum','ra','dec','st_spstr','st_age','pl_ratdor','st_rad','rowupdate']
 reader = csv.DictReader(csvfile, fieldnames)
 
+output = "["
+
 for row in reader:
-    jsonfile = open(jsondir + '/' + row['pl_name'].replace(' ', '') + '.json', 'w+')
-    out = json.dumps(row)
-    jsonfile.write(out)
+    # jsonfile = open(jsondir + '/' + row['pl_name'].replace(' ', '') + '.json', 'w+')
+    if reader.line_num == 1:
+        continue
+    output = output + json.dumps(row, separators=(',',':')) + ","
+
+output = output[0:-1]
+output = output + "]"
+
+all_data = json.loads(output)
+
+keys_to_delete = []
+
+for row in all_data:
+    line = all_data.index(row)
+    for key in row:
+        if row[key] == "":
+            keys_to_delete.append({"row": line, "key": key})
+
+for i in keys_to_delete:
+    obj = all_data[i["row"]]
+    obj.pop(i["key"], None)
+
+out = json.dumps(all_data)
+
+jsonfile.write(out)
